2ea98bb73bfa64c2fa79d53914bcd29c28674e45
Micael Morais <micaelmor@gmail.com>
2020-05-03 15:27:35 +0100

    Initial Commit

---
+++ CsharpProject/ProgramEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CsharpProject;
+using FsharpProject;
+
+namespace OrdenadosControlLibrary
+{
+    class ProgramEntry
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // This is the program entry which calls a function in the FsharpProject that is essentially the initialization of a ViewModel sort of thing.
+            Elmish.WPF.Program.runWindow(new MainWindow(), ChangeCounterHere.Main(args));
+        }
+    }
+}
---
+++ FsharpProject/ChangeCounterHere.fs
@@ -0,0 +1,41 @@
+﻿namespace FsharpProject
+
+module ChangeCounterHere =
+
+    open System
+    open Elmish
+    open Elmish.WPF
+
+    type Model = {
+        Counter: int
+        }
+
+    let init() = {
+        Counter = 1 // Changing this value changes the initial counter value, which can be checked by pressing the Reset button.
+        }
+        
+    type Msg =
+        | AddToCounter
+        | SubtractFromCounter
+        | Reset
+
+    let GetIncrement () =
+        1 // Changing this value changes the increment ammount, which can be checked by pressing either the + or the - button.
+
+    let update msg m =
+        match msg with
+        | AddToCounter -> {m with Counter = m.Counter + GetIncrement()}
+        | SubtractFromCounter -> {m with Counter = m.Counter - GetIncrement()}
+        | Reset -> init()
+
+    let bindings() =
+        [
+        "Counter" |> Binding.oneWay (fun m -> m.Counter)
+        "AddToCounter" |> Binding.cmd AddToCounter
+        "SubtractFromCounter" |> Binding.cmd SubtractFromCounter
+        "Reset" |> Binding.cmd Reset
+        ]
+
+
+    // This is the function that gets called by the C# project to initialize a kind of "ViewModel" to serve the C# WPF.
+    let Main argv = Program.mkSimpleWpf init update bindings
